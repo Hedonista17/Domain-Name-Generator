f5c0cf37478ebb553e3811e0c0d683571ade078c
Hedonista17 <andreaynereapiza@gmail.com>
2023-02-14 19:04:13 +0000

    Realizacion de ejercicio domain generator javascript

---
+++ app.js
@@ -0,0 +1,14 @@
+let pronom = ["the", "our", "us", "them"];
+let adjetivos = ["great", "big", "small", "sadest"];
+let nombres = ["jogger", "racoon", "turttle", "horse"];
+let dominios = [".net", ".com", ".es", ".uk"];
+
+pronom.forEach((item) => {
+  adjetivos.forEach((item2) => {
+    nombres.forEach((item3) => {
+      dominios.forEach((item4) => {
+        console.log(item,item2,item3,item4);
+      });
+    });
+  });
+});
