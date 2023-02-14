df8615565f200c7b41a15953c473a464d3e90aa8
Hedonista17 <andreaynereapiza@gmail.com>
2023-02-14 19:59:47 +0000

    se modifican archivos app.js eindex.html

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const div = document.querySelector("#domain");
+  let pronom = ["the", "our", "us", "them"];
+  let adjetivos = ["great", "big", "small", "sadest"];
+  let nombres = ["jogger", "racoon", "turttle", "horse"];
+  let dominios = [".net", ".com", ".es", ".uk"];
+
+  pronom.forEach(item => {
+    adjetivos.forEach(item2 => {
+      nombres.forEach(item3 => {
+        dominios.forEach(item4 => {
+          console.log(`${item}${item2}${item3}${item4}`);
+        });
+      });
+    });
+  });
+
+  div.innerHTML = "Generador";
 };
